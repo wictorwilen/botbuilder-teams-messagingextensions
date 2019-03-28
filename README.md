dea4771fb01adb921a95993499220d0661fb9dbc
Wictor Wilen <wictor@wictorwilen.se>
2019-03-28 21:01:49 +0100

    Initial checkin

---
+++ README.md
@@ -0,0 +1,21 @@
+# Microsoft Teams Messaging Extension Middleware for Microsoft Bot Builder 
+
+[![npm version](https://badge.fury.io/js/botbuilder-teams-messagingextensions.svg)](https://badge.fury.io/js/botbuilder-teams-messagingextensions)
+
+This middleware for [Bot Builder Framework](TODO: ) is targeted for [Microsoft Teams](https://docs.microsoft.com/en-us/microsoftteams/platform/) based bots.
+
+ | @master | @preview |
+ :--------:|:---------:
+ [![Build Status](https://travis-ci.org/wictorwilen/botbuilder-teams-messagingextensions.svg?branch=master)](https://travis-ci.org/wictorwilen/botbuilder-teams-messagingextensions)|[![Build Status](https://travis-ci.org/wictorwilen/botbuilder-teams-messagingextensions.svg?branch=preview)](https://travis-ci.org/wictorwilen/botbuilder-teams-messagingextensions)
+
+TODO: 
+
+## Usage
+
+TODO: 
+
+## License
+
+Copyright (c) Wictor Wilén. All rights reserved.
+
+Licensed under the MIT license.
---
+++ src/index.ts
@@ -0,0 +1,131 @@
+// Copyright (c) Wictor Wilén. All rights reserved.
+// Licensed under the MIT license.
+
+import { Middleware, TurnContext } from "botbuilder";
+import { ActivityTypesEx, MessagingExtensionQuery, MessagingExtensionResult } from "botbuilder-teams";
+
+/**
+ * Defines the processor for the Messaging Extension Middleware
+ */
+export interface IMessagingExtensionMiddlewareProcessor {
+    /**
+     * Processes incoming queries
+     * @param context the turn context
+     * @param value the value of the query
+     */
+    onQuery(context: TurnContext, value: MessagingExtensionQuery): Promise<MessagingExtensionResult>;
+    /**
+     * Process incoming requests for Messaging Extension settings
+     * @param context the turn context
+     */
+    onQuerySettingsUrl(context: TurnContext): Promise<{ title: string, value: string }>;
+    /**
+     * Processes incoming setting updates
+     * @param context the turn context
+     */
+    onSettings(context: TurnContext): Promise<void>;
+}
+
+/**
+ * A Messaging Extension Middleware for Microsoft Teams
+ */
+export class MessagingExtensionMiddleware implements Middleware {
+
+    /**
+     * Default constructor
+     * @param commandId The commandIf of the Messaging Extension to process,
+     *                  or `undefined` to process all incoming requests
+     * @param processor The processor
+     */
+    public constructor(
+        private commandId: string | undefined,
+        private processor: IMessagingExtensionMiddlewareProcessor) {
+
+    }
+
+    public async onTurn(context: TurnContext, next: () => Promise<void>): Promise<void> {
+        switch (context.activity.name) {
+            case "composeExtension/query":
+                if (this.commandId === context.activity.value.commandId || this.commandId === undefined) {
+                    try {
+                        const result = await this.processor.onQuery(context, context.activity.value);
+                        context.sendActivity({
+                            type: ActivityTypesEx.InvokeResponse,
+                            value: {
+                                body: {
+                                    composeExtension: result,
+                                },
+                                status: 200,
+                            },
+                        });
+                    } catch (err) {
+                        context.sendActivity({
+                            type: ActivityTypesEx.InvokeResponse,
+                            value: {
+                                body: err,
+                                status: 500,
+                            },
+                        });
+                    }
+                }
+                break;
+            case "composeExtension/querySettingUrl":
+                if (this.commandId === context.activity.value.commandId || this.commandId === undefined) {
+                    try {
+                        const result = await this.processor.onQuerySettingsUrl(context);
+                        context.sendActivity({
+                            type: ActivityTypesEx.InvokeResponse,
+                            value: {
+                                body: {
+                                    composeExtension: {
+                                        suggestedActions: {
+                                            actions: [{
+                                                type: "openApp",
+                                                ...result,
+                                            }],
+                                        },
+                                        type: "config",
+                                    },
+                                },
+                                status: 200,
+                            },
+                        });
+                    } catch (err) {
+                        context.sendActivity({
+                            type: ActivityTypesEx.InvokeResponse,
+                            value: {
+                                body: err,
+                                status: 500,
+                            },
+                        });
+                    }
+                }
+                break;
+            case "composeExtension/setting":
+                if (this.commandId === context.activity.value.commandId || this.commandId === undefined) {
+                    try {
+                        await this.processor.onSettings(context);
+                        context.sendActivity({
+                            type: ActivityTypesEx.InvokeResponse,
+                            value: {
+                                status: 200,
+                            },
+                        });
+                    } catch (err) {
+                        context.sendActivity({
+                            type: ActivityTypesEx.InvokeResponse,
+                            value: {
+                                body: err,
+                                status: 500,
+                            },
+                        });
+                    }
+                }
+                break;
+            default:
+                // nop
+                break;
+        }
+        return next();
+    }
+}
